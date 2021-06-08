036548022d3f3467e6a4ff689a3490e564c781f1
Igor Aherne <igor.aherne.videos@gmail.com>
2021-06-08 15:50:12 +0100

    removed RawData_Remainder because it's obsolete and isn't actually used

--- RawData_Buff.h
+++ RawData_Buff.h
@@ -6,46 +6,6 @@
 #include <memory>
 #include <cassert>
 
-class RawData_Buff;
-class RawData_Remainder;
-
-
-// Reader can unload bytes into here from the end of 'RawData_Buff'.
-// This is useful when there isn't enough data at the current buffer, 
-// and this remainder has to be kept aside until next buffer is available.
-class RawData_Remainder {
-    void cleanup(){
-        if(_data != nullptr){  _aligned_free(_data);  }
-        _size = 0;
-    }
-    RawData_Remainder(const RawData_Remainder& other) = delete;
-    RawData_Remainder& operator=(const RawData_Remainder& other) = delete;
-
-public:
-    RawData_Remainder(size_t maxNumBytes){
-        _data =  (unsigned char*)_aligned_malloc(maxNumBytes, 16);
-        _allocatedSize = maxNumBytes;
-        _size = 0;
-    }
-
-    ~RawData_Remainder(){
-        cleanup();
-    }
-   
-
-    unsigned char* data(){ return _data; }
-    void clear(){ cleanup(); }
-    bool isEmpty()const{ return _size==0; }
-    size_t size(){ return _size; }
-
-private:
-    unsigned char* _data = nullptr;
-    size_t _size = 0;//less than or equal to '_allocatedSize'
-    size_t _allocatedSize = 0;
-};
-
-
-
 // reader can unload bytes into here.
 // You can then read items from it.
 class RawData_Buff {
